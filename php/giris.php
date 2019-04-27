f3693ceb0002f46643d881c0589031caa39bb8f6
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-04-27 19:43:12 +0300

    giriş sayfasının php kısmı oluşturuldu.
    kayıt olan müşterilerin kayitları sorgulanarak siteye erişimi sağlandı.

---
+++ php/giris.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'baglan.php';
+
+if ($_POST){
+	
+	if (isset( $_POST["giris_mail"])){
+		$giris_mail=strip_tags(trim( $_POST["giris_mail"]));
+	}
+	else{
+		echo "Ad / Soyad requesti olmadı.";
+	}
+
+	if (isset( $_POST["giris_pass"])){
+		$giris_pass =strip_tags(trim( $_POST["giris_pass"]));
+	}
+	else{
+		echo "Ad / Soyad requesti olmadı.";
+	}
+
+	$sorgu=mysqli_query($baglan,"select * from kullanici_kayit where e_posta='$giris_mail' and parola='$giris_pass'");
+
+	if(mysqli_num_rows($sorgu)>0){
+		echo "Hoş Geldiniz";
+	}else{
+		echo "Kullanıcı adı veya parola yanlış..!";
+	}
+}
+?>
