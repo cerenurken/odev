d0c03bda81f69d091e544ceabf14935375e8fad5
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-01 12:57:48 +0300

    giderler.php sayfası oluşturuldu
    ürün_giris alanındaki gider kalemleri için giderler.php sayfası oluşturularak veritabı erişimi sağlandı.

---
+++ php/giderler.php
@@ -0,0 +1,56 @@
+<?php
+	require 'baglan.php';
+
+	if ($_GET){
+
+		if(isset($_GET["fatura"])){
+			$fatura_g = $_GET["fatura"];
+		}
+		else{
+			echo "Fatura requesti olmadı";
+		}
+
+		if(isset($_GET["odeme"])){
+			$odeme_g = $_GET["odeme"];
+		}
+		else{
+			echo "Ödeme requesti olmadı";
+		}
+
+		if(isset($_GET["arac"])){
+			$arac_g = $_GET["arac"];
+		}
+		else{
+			echo "Araç gideri requesti olmadı";
+		}
+
+		if(isset($_GET["ofis"])){
+			$ofis_g = $_GET["ofis"];
+		}
+		else{
+			echo "Ofis gideri requesti olmadı";
+		}
+
+		if(isset($_GET["ilac"])){
+			$ilac_g = $_GET["ilac"];
+		}
+		else{
+			echo "İlaç gideri requesti olmadı";
+		}
+
+		$toplam = $fatura_g + $odeme_g + $arac_g + $ofis_g + $ilac_g ;
+
+		$giderler = "insert into odemeler (fatura,calisan,arac,ofis,ilaclama,toplam_odeme) values ('$fatura_g','$odeme_g','$arac_g','$ofis_g','$ilac_g','$toplam')";
+
+		if ($baglan->query($giderler)){
+			echo "kayıt yapıldı";
+		}
+		else{
+			echo " kayıt yapılamadı :(";
+		}
+	}
+	else{
+		die("veritabanı bağlantısı yok");
+	}
+
+?>
