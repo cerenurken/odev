9fe8712a7f83aefc431bb9f0c44ec48fef2903a0
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-04 15:01:07 +0300

    merkez bankasından enflasyon oranları çekildi.

--- admin.php
+++ admin.php
@@ -136,7 +136,7 @@
                     <div class="col-xl-2 col-md-4 col-6">
                       <div class="wrapper count-title d-flex">
                         <div class="icon "><i class="fas fa-chart-line gosterge-icon"></i></div>
-                        <div class="name"><h5 class="text-uppercase ust-baslik">BORSA HAREKETLERİ</h5>
+                        <div class="name"><h5 class="text-uppercase ust-baslik">ENFLASYON ORANI</h5>
                              <h6 class="orta-baslik">Merkez Bankası</h6>
                         </div>
                       </div>
@@ -184,10 +184,9 @@
                      <!-- Count item widget-->
                     <div class="col-xl-2 col-md-4 col-6">
                       <div class="wrapper count-title d-flex">
-                        <div class="icon"><i class="far fa-file-alt gosterge-icon"></i></div>
-                        <div class="name"><h5 class="text-uppercase ust-baslik">Faturalar</h5>
-                           <center> <h6 class="orta-baslik">Son 25 Gün</h6>
-                          <div class="count-number">Su-92 tl</div></center>
+                        <div class="icon"><i class="fas fa-chart-line gosterge-icon"></i></div>
+                        <div class="name"><h5 class="text-uppercase ust-baslik">BORSA HAREKETLERİ</h5>
+                           <center> <h6 class="orta-baslik">Merkez Bankası</h6>
                         </div>
                       </div>
                     </div>
@@ -201,78 +200,81 @@
             <!--<h2>Anasayfa</h2>-->
 
             <div class="row">
-                <div class="col-sm-12 col-md-6" style="margin-bottom: 3%;">
+                <div class="col-sm-12 col-md-6" style="margin-bottom: 5%; margin-top: 1.7%;">
                     <div class="card" style="margin-bottom: 0.3%;">
-                        <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">BORSA HAREKETLERİ</h4>
+                        <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">YILLIK VE AYLIK ENFLASYON ORANI</h4>
                     </div>
                     <div class="card text-left">
                         <?php
-                            $connect_web = simplexml_load_file('http://www.tcmb.gov.tr/kurlar/today.xml');
-                            $usd_buying = $connect_web->Currency[0]->BanknoteBuying;
-                            $usd_selling = $connect_web->Currency[0]->BanknoteSelling;
-                            $euro_buying = $connect_web->Currency[3]->BanknoteBuying;
-                            $euro_selling = $connect_web->Currency[3]->BanknoteSelling;
-                            $dnr_buying = $connect_web->Currency[4]->BanknoteBuying;
-                            $dnr_selling = $connect_web->Currency[4]->BanknoteSelling;
-                            $str_buying = $connect_web->Currency[8]->BanknoteBuying;
-                            $str_selling = $connect_web->Currency[8]->BanknoteSelling;
-                            $jpn_buying = $connect_web->Currency[11]->BanknoteBuying;
-                            $jpn_selling = $connect_web->Currency[11]->BanknoteSelling;
+                            function getir($baslangic, $son, $cekilmek_istenen)
+{
+    @preg_match_all('/' . preg_quote($baslangic, '/') .
+    '(.*?)'. preg_quote($son, '/').'/i', $cekilmek_istenen, $m);
+    return @$m[1];
+}
 
+$url = "https://www.tcmb.gov.tr/wps/wcm/connect/TR/TCMB+TR/Main+Menu/Istatistikler/Enflasyon+Verileri/Tuketici+Fiyatlari";
+$cekilen_veri = file_get_contents($url);
+$ay = getir('<td style="height: 20px; width: 120px;">',"</td>",$cekilen_veri);
+$yıl=getir('<td style="text-align: center; width: 140px;">',"</td>",$cekilen_veri);
+$tufe=getir('<td style="text-align: center;">',"</td>",$cekilen_veri);
                             echo "<table class='table'>";
                             echo "<thead class='thead-dark'>";
                             echo "<tr>";
-                            echo "<th scope='col' style='padding-left:16%;'><i class='fas fa-lira-sign'></i>ALIŞ</th>";
-                            echo "<th scope='col'><i class='fas fa-lira-sign'></i>SATIŞ</th>";
+                            echo "<th scope='col'style='margin-left:10px;' >   <i class='far fa-calendar-alt'></i></th>";
+                            echo "<th scope='col' ><i class='fas fa-percent'></i> YILLIK ENFLASYON</th>";
+                            echo "<th scope='col'><i class='fas fa-percent'></i> AYLIK ENFLASYON</th>";
                              echo "</tr>";
                             echo "</thead>";
                             echo "<tbody>";
                             echo "<tr>";
-                            echo "<td ><i class='fas fa-euro-sign' ></i><span style ='color:#c3c3c3; margin-right:17px;'> EURO :</span> <label> ".$euro_buying."</label></td>";
-                            echo "<td>"  .$euro_selling."</td>";
+                            echo "<td > ".$ay[0]."</td>";
+                            echo "<td style='padding-left:60px;'> ".$yıl[0]."</td>";
+                            echo "<td style='padding-left:70px;'>"  .$tufe[0]."</td>";
                             echo "</tr>";
                             echo "<tr>";
-                            echo "<td><i class='fas fa-dollar-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> DOLAR :</span> <label>".$usd_buying."</label></td>";
-                            echo "<td>".$usd_selling."</td>";
+                            echo "<td > ".$ay[1]."</td>";
+                            echo "<td style='padding-left:60px;'> ".$yıl[1]."</td>";
+                            echo "<td style='padding-left:70px;'>"  .$tufe[1]."</td>";
                             echo "</tr>";
                             echo "<tr>";
-                            echo "<td><i class='fas fa-pound-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> STERLİN :</span> <label>".$str_buying."</label></td>";
-                            echo "<td>".$str_selling."</td>";
+                            echo "<td > ".$ay[2]."</td>";
+                            echo "<td style='padding-left:60px;'> ".$yıl[2]."</td>";
+                            echo "<td style='padding-left:70px;'>"  .$tufe[2]."</td>";
                             echo "</tr>";
                             echo "<tr>";
-                            echo "<td><i class='fas fa-yen-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> JAPON :</span> <label>".$jpn_buying."</label></td>";
-                            echo "<td>".$jpn_selling."</td>";
+                            echo "<td > ".$ay[3]."</td>";
+                            echo "<td style='padding-left:60px;'> ".$yıl[3]."</td>";
+                            echo "<td style='padding-left:70px;'>"  .$tufe[3]."</td>";
                             echo "</tr>";
                             echo "<tr>";
-                            echo "<td>د.ك <span style ='color:#c3c3c3; margin-right:14px;'> DİNAR :</span> <label>".$dnr_buying."</label></td>";
-                            echo "<td>".$dnr_selling."</td>";
+                            echo "<td > ".$ay[4]."</td>";
+                            echo "<td style='padding-left:60px;'> ".$yıl[4]."</td>";
+                            echo "<td style='padding-left:70px;'>"  .$tufe[4]."</td>";
                             echo "</tr>";
                             echo "</table>";
 
-
-
                         ?>
+
+
+
                     </div>
                 </div>
 
-                <div class="col-sm-12 col-md-6" style="margin-bottom: 3%;">
+                <div class="col-sm-12 col-md-6" style="margin-bottom: 5%; margin-top: 1.7%;">
                     <div class="card" style="margin-bottom: 0.3%;">
                         <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">GÜNLÜK SATIŞ GELİRİ</h4>
                     </div>
-                    <div class="card text-left">
-                        <div class="card-header">Müşterilerin Sık Ziyaret Ettiği Kısımlar</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        <div class="card-footer text-muted">2 days ago</div>
+                    <div class="card" style="width: 600px; height: 310px;">
+                        <video style="width: 570px; height: 280px; padding-top: 4%;" controls>
+                            <source src="resimler/organik.mp4"type="video/mp4">
+                        </video>
                     </div>
                 </div>
 
 
                 
-                <div class="col-sm-12 col-md-6" style="margin-bottom: 3%;">
+                <div class="col-sm-12 col-md-6" style="margin-bottom: 5%; margin-top: 1.7%;">
                     <div class="card" style="margin-bottom: 0.3%;">
                         <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">YENİ MÜŞTERİLER</h4>
                     </div>
@@ -301,7 +303,7 @@
                     </div>
                 </div>
 
-                <div class="col col-sm-12 col-md-6 " style="margin-bottom: 3%;">
+                <div class="col col-sm-12 col-md-6 " style="margin-bottom: 5%; margin-top: 1.7%;">
                     <div class="card" style="margin-bottom: 0.3%;">
                         <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">YENİ SİPARİŞLER</h4>
                     </div>
@@ -350,6 +352,57 @@
                         </div>
                         <div class="card-footer text-muted">2 days ago</div>
                     </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="card text-left">
+                        <div class="card" style="margin-bottom: 5%; margin-top: 1.7%;">
+                        <h4 style="padding-top: 3%; padding-bottom: 0.8%; padding-left: 3%; color: #ff7838;">TÜKETİCİ FİYAT ENDEKSİ</h4>
+                    </div>
+                <?php
+                             $connect_web = simplexml_load_file('http://www.tcmb.gov.tr/kurlar/today.xml');
+                            $usd_buying = $connect_web->Currency[0]->BanknoteBuying;
+                            $usd_selling = $connect_web->Currency[0]->BanknoteSelling;
+                            $euro_buying = $connect_web->Currency[3]->BanknoteBuying;
+                            $euro_selling = $connect_web->Currency[3]->BanknoteSelling;
+                            $dnr_buying = $connect_web->Currency[4]->BanknoteBuying;
+                            $dnr_selling = $connect_web->Currency[4]->BanknoteSelling;
+                            $str_buying = $connect_web->Currency[8]->BanknoteBuying;
+                            $str_selling = $connect_web->Currency[8]->BanknoteSelling;
+                            $jpn_buying = $connect_web->Currency[11]->BanknoteBuying;
+                            $jpn_selling = $connect_web->Currency[11]->BanknoteSelling;
+
+                            echo "<table class='table'>";
+                            echo "<thead class='thead-dark'>";
+                            echo "<tr>";
+                            echo "<th scope='col' style='padding-left:16%;'><i class='fas fa-lira-sign'></i>ALIŞ</th>";
+                            echo "<th scope='col'><i class='fas fa-lira-sign'></i>SATIŞ</th>";
+                             echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                            echo "<tr>";
+                            echo "<td ><i class='fas fa-euro-sign' ></i><span style ='color:#c3c3c3; margin-right:17px;'> EURO :</span> <label> ".$euro_buying."</label></td>";
+                            echo "<td>"  .$euro_selling."</td>";
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<td><i class='fas fa-dollar-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> DOLAR :</span> <label>".$usd_buying."</label></td>";
+                            echo "<td>".$usd_selling."</td>";
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<td><i class='fas fa-pound-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> STERLİN :</span> <label>".$str_buying."</label></td>";
+                            echo "<td>".$str_selling."</td>";
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<td><i class='fas fa-yen-sign'></i><span style ='color:#c3c3c3; margin-right:14px;'> JAPON :</span> <label>".$jpn_buying."</label></td>";
+                            echo "<td>".$jpn_selling."</td>";
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<td>د.ك <span style ='color:#c3c3c3; margin-right:14px;'> DİNAR :</span> <label>".$dnr_buying."</label></td>";
+                            echo "<td>".$dnr_selling."</td>";
+                            echo "</tr>";
+                            echo "</table>";
+
+                            ?>
+                            </div>
                 </div>
 
             </div>
