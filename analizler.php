907893dc8cb75a20ceead3c4b810114493121164
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-18 12:43:30 +0300

    grafik eklendi

--- analizler.php
+++ analizler.php
@@ -1,7 +1,65 @@
-<?php 
-    include 'menubar/head.php';
-?>
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>organicK | Admin</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/admin-panel.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+<script>
+$(document).ready(function(){
+showGraph();
+});
+function showGraph(){
+$.post("php/analiz.php",
+function(data){
+console.log(data);
+var odeme_tarihi=[];
+var toplam=[];
+for (var i in data){
+odeme_tarihi.push(data[i].odeme_tarihi);
+toplam.push(data[i].toplam);
+};
+var chartdata={
+labels:odeme_tarihi,
+datasets:[
+{
+label:'Aylık Toplam Gider',
+backgroundColor:"#db3f3f",
+data:toplam
+
+}]
+};
+var cnv=$("#myChart");
+var barGraph=new Chart(cnv,{
+type:'bar',
+data:chartdata
+});
+});
+};
+</script>
+
+
+
+</head>
 <body>
 
    <!-- mobile haeader nav  -->
@@ -99,12 +157,7 @@
                 <div class="col-sm-12 col-md-6">
                     <div class="card text-left">
                         <div class="card-header">En Çok Ziyaret Edilen Saatler</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        <div class="card-footer text-muted">2 days ago</div>
+                        <canvas id="myChart"></canvas>
                     </div>
                 </div>
 
@@ -149,8 +202,7 @@
         
 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <!-- Popper.JS -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
---
+++ php/analiz.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type:application/json');
+require 'baglan.php';
+$sorgu=mysqli_query($baglan,"SELECT odeme_tarihi,toplam FROM odemeler");
+$data=array();
+foreach($sorgu as $row){
+$data[]=$row;
+}
+mysqli_close($baglan);
+echo json_encode($data);
+?>
