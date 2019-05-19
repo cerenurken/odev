76b72de6ce96e951b0187b73f5f0329127ba2725
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-19 15:27:42 +0300

    giriş sayfasına session eklendi

--- php/giris.php
+++ php/giris.php
@@ -4,26 +4,18 @@ require 'baglan.php';
 
 if ($_POST){
 	
-	if (isset( $_POST["giris_mail"])){
+	
 		$giris_mail=strip_tags(trim( $_POST["giris_mail"]));
-	}
-	else{
-		echo "Ad / Soyad requesti olmadı.";
-	}
-
-	if (isset( $_POST["giris_pass"])){
 		$giris_pass =strip_tags(trim( $_POST["giris_pass"]));
-	}
-	else{
-		echo "Ad / Soyad requesti olmadı.";
-	}
 
-	$sorgu=mysqli_query($baglan,"select * from kullanici_kayit where e_posta='$giris_mail' and parola='$giris_pass'");
+			$sorgu=mysqli_query($baglan,"select * from kullanici_kayit where e_posta='$giris_mail' and parola='$giris_pass'");
 
-	if(mysqli_num_rows($sorgu)>0){
-		header("Location:../index.php");
-	}else{
-		echo "Kullanıcı adı veya parola yanlış..!";
+			if(mysqli_num_rows($sorgu)>0){
+				while ($row = $sorgu->fetch_assoc()) {
+					$_SESSION["user_id"]=$row["musteri_id"];
+					echo $_SESSION["user_id"];
+
+				}
 	}
 
 }
