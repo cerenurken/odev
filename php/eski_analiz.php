5e384fef7baad4ccee4487cc4d6d22887e93fa1b
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-22 00:19:02 +0300

    eski ve yeni fiyat tek tabloda birleştirildi

--- php/eski_analiz.php
+++ php/eski_analiz.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type:application/json');
 require 'baglan.php';
-$sorgu=mysqli_query($baglan,"SELECT urun_guncelleme.fiyat as eski ,urun_kayit.urun_ad as ad ,urun_guncelleme.tarih as tarih FROM urun_kayit,urun_guncelleme WHERE urun_kayit.urun_ad=urun_guncelleme.urun_ad");
+$sorgu=mysqli_query($baglan,"SELECT urun_guncelleme.fiyat as eski,urun_kayit.fiyat as yeni ,urun_kayit.urun_ad as ad FROM urun_kayit,urun_guncelleme WHERE urun_kayit.urun_ad=urun_guncelleme.urun_ad");
 $data=array();
 foreach($sorgu as $row){
 $data[]=$row;
