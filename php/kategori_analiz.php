671cc79c5b75f926a69eb8acd432143983959a01
cerenurken <36957603+cerenurken@users.noreply.github.com>
2019-05-22 00:21:03 +0300

    join ile kategorilere göre üretilen ürün miktarı ve satışlardan elde edilen gelirler belirlendi

--- php/kategori_analiz.php
+++ php/kategori_analiz.php
@@ -1,7 +1,13 @@
 <?php
 header('Content-Type:application/json');
 require 'baglan.php';
-$sorgu=mysqli_query($baglan,"SELECT sum(stok_miktari) as sayi,kategori_adi as kategori FROM urun_kayit,kategori where urun_kayit.kategori_id = kategori.kategori_id group by kategori");
+$sorgu=mysqli_query($baglan,"SELECT SUM(siparisler.miktar*siparisler.birim_fiyat) as getiri,kategori.kategori_adi as ad,sum(urun_kayit.stok_miktari) as toplam
+FROM kategori
+LEFT JOIN urun_kayit
+ON urun_kayit.kategori_id=kategori.kategori_id
+LEFT JOIN siparisler
+on urun_kayit.urun_id=siparisler.urun_id
+GROUP BY kategori.kategori_id");
 $data=array();
 foreach($sorgu as $row){
 $data[]=$row;
